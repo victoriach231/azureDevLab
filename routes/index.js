08cb18b815dbebc6ba49f874166d1622b63ae1a4
victoriach231 <victoriach231@gmail.com>
2023-03-31 12:29:47 -0500

    changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Deployment Lab' });
 });
 
 module.exports = router;
